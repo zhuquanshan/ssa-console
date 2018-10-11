6e36227ff9466657ca47244f171745f3860a5223
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-11 22:53:34 +0000

    Deploy d2-projects/d2-admin-start-kit to github.com/d2-projects/d2-admin-start-kit.git:gh-pages

--- js/chunk-2d22a0ea.fbaf8ceb.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22a0ea"],{dfa3:function(e,n,r){"use strict";r.r(n);r("a481");var a,t,u={beforeCreate:function(){var e=this.$route.params.path;this.$router.replace(JSON.parse(e))},render:function(e){return e()}},o=u,i=r("2877"),p=Object(i["a"])(o,a,t,!1,null,null,null);p.options.__file="index.vue";n["default"]=p.exports}}]);
-//# sourceMappingURL=chunk-2d22a0ea.fbaf8ceb.js.map
